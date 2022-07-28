4537df73960ed62309299f3e5b7205064bca6c4f
wameny10814 <wameny10814@gmail.com>
2022-07-28 19:25:59 +0800

    0728 REG 後端 DONE & MEM 前端回傳

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const yuupload = require(__dirname + "/modules/yu-upload-images");
 //upload要改名字
 const upload = require(__dirname + "/modules/upload-images");
 
-
 const session = require("express-session");
 const moment = require("moment-timezone");
 const axios = require("axios");
@@ -35,7 +34,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-
 app.use(
     session({
         saveUninitialized: false,
@@ -54,10 +52,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //yuchen 登入----------------------------------------------------------
-app.get('/',function (req,res){
-    res.send('hellw');
-})
-app.use((req, res, next)=>{
+app.get("/", function (req, res) {
+    res.send("hellw");
+});
+app.use((req, res, next) => {
     // res.locals.shinder = '哈囉';
 
     // template helper functions
@@ -65,17 +63,16 @@ app.use((req, res, next)=>{
     res.locals.toDatetimeString = toDatetimeString;
     res.locals.session = req.session;
 
-
     //解密寫在middle ware 才可以全部頁面都拿到token
-    const auth = req.get('Authorization');
+    const auth = req.get("Authorization");
     res.locals.payload = null;
-    if(auth && auth.indexOf('Bearer ')===0){
-    const token = auth.slice(7);
-    //jwt判定完成存進locals.payload 裡面
-    //2022-07-20 15 36 28 如果路由需要確認有沒有登入才做的話
-    //jwt 每次request 都需要給token
-    res.locals.payload = jwt.verify(token, process.env.JWT_SECRET);
-        }
+    if (auth && auth.indexOf("Bearer ") === 0) {
+        const token = auth.slice(7);
+        //jwt判定完成存進locals.payload 裡面
+        //2022-07-20 15 36 28 如果路由需要確認有沒有登入才做的話
+        //jwt 每次request 都需要給token
+        res.locals.payload = jwt.verify(token, process.env.JWT_SECRET);
+    }
 
     next();
 });
@@ -129,57 +126,60 @@ const addressbook = require(__dirname + "/routes/address-book1");
 // prefix 前綴路徑
 app.use("/address-book", addressbook);
 
-
 //yuchen router--------------------------------------------------
 const memberRouter = require(__dirname + "/routes/member");
 app.use("/member", memberRouter);
 
-
-
-
 //yuchen 登入-----------------------------------------
-app.route('/login-jwt')
-    .get(async (req, res)=>{
-        res.render('login-jwt');
+app.route("/login-jwt")
+    .get(async (req, res) => {
+        res.render("login-jwt");
     })
-    .post(async (req, res)=>{
+    .post(async (req, res) => {
         const output = {
             success: false,
-            error: '',
+            error: "",
             code: 0,
             data: {},
         };
         //用帳號撈會員資料
-        const sql = "SELECT * FROM admin WHERE account=?";
+        const sql = "SELECT * FROM member WHERE account=?";
         const [r1] = await db.query(sql, [req.body.account]);
 
-        if(! r1.length){
+        if (!r1.length) {
             // 帳號錯誤
             output.code = 401;
-            output.error = '帳密錯誤'
-            return res.json(output)
+            output.error = "帳密錯誤";
+            return res.json(output);
         }
         //const row = r1[0];
         //密碼對比
-        output.success = await bcrypt.compare(req.body.password, r1[0].pass_hash);
-        if(! output.success){
+        output.success = await bcrypt.compare(
+            req.body.password,
+            r1[0].pass_hash
+        );
+        if (!output.success) {
             // 密碼錯誤
             output.code = 402;
-            output.error = '帳密錯誤'
+            output.error = "帳密錯誤";
         } else {
             // 成功登入
             //登入成功生成token
-            const token = jwt.sign({
-                sid: r1[0].sid,
-                account: r1[0].account,
-            }, process.env.JWT_SECRET);
-
+            const token = jwt.sign(
+                {
+                    sid: r1[0].sid,
+                    account: r1[0].account,
+                    level: r1[0].level,
+                },
+                process.env.JWT_SECRET
+            );
 
             output.data = {
                 token,
+                sid: r1[0].sid,
                 account: r1[0].account,
+                level: r1[0].level,
             };
-
         }
 
         res.json(output);
@@ -190,9 +190,6 @@ app.get("/logout", (req, res) => {
     delete req.session.admin;
     res.redirect("/");
 });
-
-
-
 
 // ------- static folder -----------
 app.use(express.static("public"));
--- routes/member.js
+++ routes/member.js
@@ -1,5 +1,5 @@
-const express = require('express');
-
+const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router(); // 建立 router 物件
 const db = require(__dirname + "/../modules/mysql-connect");
 
@@ -7,10 +7,8 @@ const db = require(__dirname + "/../modules/mysql-connect");
 //        res.render('address-book/add');
 //     });
 
-
 //c
-router.post('/add', async (req, res)=>{
-   
+router.post("/add", async (req, res) => {
     // if(! req.session.admin){
     //     return res.redirect('/');
     // }
@@ -30,13 +28,26 @@ router.post('/add', async (req, res)=>{
     // https://stackoverflow.com/questions/48720942/node-js-joi-how-to-display-a-custom-error-messages
 
     // console.log( schema.validate(req.body, {abortEarly: false}) );
-    const sql ="INSERT INTO `member`(`account`, `pass_hash`, `birthday`, `email`, `level`, `creat_at`) VALUES (?,?,?,?,1,NOW())"
-    const {account, pass_hash,birthday,email} = req.body;
-    const [result] = await db.query(sql, [account, pass_hash, birthday, email]);
+    const sql =
+        "INSERT INTO `member`(`account`, `pass_hash`, `birthday`, `email`, `level`, `creat_at`) VALUES (?,?,?,?,1,NOW())";
+    const { account, pass_hash, birthday, email } = req.body;
+    // const saltRounds = 10;
+    // const myPlaintextPassword = "s0//P4$$w0rD";
+    var hash = await bcrypt.hash(pass_hash, 10);
+    // bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+    //     // Store hash in your password DB.
+    // });
+    const [result] = await db.query(sql, [account, hash, birthday, email]);
 
-    
     res.json(result);
+});
 
+router.get("/userdata", async (req, res) => {
+    const sql = `SELECT sid, account, pass_hash, name, birthday, email, mobile, address, avatar, level, creat_at FROM member WHERE sid=${res.locals.payload.sid}`;
+    // const sql2 =SELECT `sid`, `account`, `pass_hash`, `name`, `birthday`, `email`, `mobile`, `address`, `avatar`, `level`, `creat_at` FROM `member` WHERE sid =1;
+    const [result] = await db.query(sql, [req.body]);
+
+    res.json(result);
 });
 
 module.exports = router;

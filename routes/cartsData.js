0ad5bf8007c0c1b922d12c6ad3b91cd5c37ae4d5
Shuhao <loveyu5566@outlook.com>
2022-08-20 17:15:22 +0800

    建置寫入資料庫ing

--- routes/cartsData.js
+++ routes/cartsData.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require(__dirname + "/../modules/mysql-connect");
 
-
 const router = express.Router(); // 建立 router 物件
 
 // loveyu 購物車寫入資料庫 ----------------------------
@@ -14,7 +13,39 @@ const router = express.Router(); // 建立 router 物件
 //     const [result]=await db.query(sql, [total_quantity,pay_price,ship_name,ship_phone,ship_email,ship_address]);
 //     res.json(result)
 // });
-    
 
+router.post("/cartsData", async (req, res) => {
+  const {
+    memSid ,
+    created_at,
+    count,
+    product_price,
+    shipName,
+    shipPhone,
+    shipEmail,
+    addressDetail,
+  } = req.body;
+  let output = {
+    success: false,
+    data: req.body,
+    error: "",
+    information: "",
+  };
+  if (!!req.body === true) {
+    const sql =
+      "INSERT INTO `cart_orders`(`member_sid`, `created_at`, `total_quantity`, `pay_price`, `pay_type`, `store_name`, `market_name`, `ship_name`, `ship_phone`, `ship_email`, `ship_address`, `discount_code`) VALUES (?,NOW(),?,?,?,?,?,?,?,?,?,?)";
+    const [result] = await db.query(sql, [
+      mem,
+      img,
+      donut,
+      layer,
+      decoration,
+      price,
+    ]);
+    output.success = true;
+    output.information = "insert ok";
+  }
+  res.json(output);
+});
 
 module.exports = router;

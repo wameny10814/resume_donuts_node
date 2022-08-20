ebbe77e6080e9d319fe99925e4660e249f82a539
Shuhao <loveyu5566@outlook.com>
2022-08-20 21:30:38 +0800

    寫入訂單到資料庫

--- routes/cartsData.js
+++ routes/cartsData.js
@@ -6,17 +6,10 @@ const db = require(__dirname + "/../modules/mysql-connect");
 const router = express.Router(); // 建立 router 物件
 
 // loveyu 購物車寫入資料庫 ----------------------------
-// router.post("/cartsData", (req, res) => {
-//   const sql =
-//     "INSERT INTO `cart_orders`(`member_sid`, `created_at`, `total_quantity`, `pay_price`, `pay_type`, `store_name`, `market_name`, `ship_name`, `ship_phone`, `ship_email`, `ship_address`, `discount_code`) VALUES (?,NOW(),?,?,?,?,?,?,?,?,?,?)";
-//     const {total_quantity,pay_price,ship_name,ship_phone,ship_email,ship_address} =req.body;
-//     const [result]=await db.query(sql, [total_quantity,pay_price,ship_name,ship_phone,ship_email,ship_address]);
-//     res.json(result)
-// });
 
 router.post("/cartsData", async (req, res) => {
   const {
-    memSid ,
+    memSid,
     created_at,
     count,
     product_price,
@@ -33,14 +26,17 @@ router.post("/cartsData", async (req, res) => {
   };
   if (!!req.body === true) {
     const sql =
-      "INSERT INTO `cart_orders`(`member_sid`, `created_at`, `total_quantity`, `pay_price`, `pay_type`, `store_name`, `market_name`, `ship_name`, `ship_phone`, `ship_email`, `ship_address`, `discount_code`) VALUES (?,NOW(),?,?,?,?,?,?,?,?,?,?)";
+      //   "INSERT INTO `cart_orders`(`member_sid`, `created_at`, `total_quantity`, `pay_price`, `pay_type`, `store_name`, `market_name`, `ship_name`, `ship_phone`, `ship_email`, `ship_address`, `discount_code`) VALUES (?,NOW(),?,?,?,?,?,?,?,?,?,?)";
+      "INSERT INTO `cart_orders`(`member_sid`, `created_at`, `total_quantity`, `pay_price`, `ship_name`, `ship_phone`, `ship_email`, `ship_address`) VALUES (?,NOW(),?,?,?,?,?,?)";
     const [result] = await db.query(sql, [
-      mem,
-      img,
-      donut,
-      layer,
-      decoration,
-      price,
+      memSid,
+      created_at,
+      count,
+      product_price,
+      shipName,
+      shipPhone,
+      shipEmail,
+      addressDetail,
     ]);
     output.success = true;
     output.information = "insert ok";
